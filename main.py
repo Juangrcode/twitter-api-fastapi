fc59088ae5a07cc619b3f8fe416c6b10c5a09199
Juangrcode <j3045771061@gmail.com>
2022-12-14 10:26:16 -0500

    :sparkles: [Add] Register functions

--- main.py
+++ main.py
@@ -37,6 +37,12 @@ class User(UserBase):
     )
     birth_date: Optional[date] = Field(default=None)
 
+class UserRegister(User):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
 
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)
@@ -64,8 +70,22 @@ class Tweet(BaseModel):
     tags=["Users"]
 )
 def signup():
-    pass
+    """
+    Signup
 
+    This path operation register a user in the app
+
+    Parameters:
+      - Request body parameter
+        - user: UserRegister
+    
+    Returns a json with the basic information:
+      - user_id: UUID
+      - email: EmailStr
+      - first_name: str
+      - last_name: str
+      - birth_name: str
+    """
 
 ### Login User
 @app.post(
