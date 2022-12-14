d8add469b028e08069924d8896fa215c6e73472f
Juangrcode <j3045771061@gmail.com>
2022-12-13 20:49:54 -0500

    :sparkles: [Add] models tweets

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Python
-from datetime import date
+from datetime import date, datetime
 from typing import Optional
 from uuid import UUID
 
@@ -39,7 +39,15 @@ class User(UserBase):
 
 
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 @app.get(
     path="/"
