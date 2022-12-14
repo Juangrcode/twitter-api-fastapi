9826e42f0cbb8e69fd3bfa9b90707a98055ec342
Juangrcode <j3045771061@gmail.com>
2022-12-13 20:45:16 -0500

    :sparkles: [Add] models

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
+# Python
+from datetime import date
+from typing import Optional
+from uuid import UUID
+
+# FastAPI
 from fastapi import FastAPI
+# Pydantic
+from pydantic import BaseModel, EmailStr, Field
 
 app = FastAPI()
+
+
+# Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+
+class Tweet(BaseModel):
+    pass
 
 @app.get(
     path="/"
