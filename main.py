db21188feb567580433e2d8c45569e8e01ad2914
Juangrcode <j3045771061@gmail.com>
2022-12-20 11:39:54 -0500

    :sparkles: [Add] Create tweet post

--- main.py
+++ main.py
@@ -122,7 +122,23 @@ def login():
     tags=["Users"]
 )
 def show_all_users():
-    pass
+    """
+    This path operation shows all users in the app
+
+    Parameters:
+      - 
+    
+    Returns a json list with all users in the app, with the following keys:
+      - user_id: UUID
+      - email: EmailStr
+      - first_name: str
+      - last_name: str
+      - birth_date: str
+    """
+
+    with open("users.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 ### Show a User
 @app.get(
@@ -180,8 +196,38 @@ def home():
     summary="Post a Tweet",
     tags=["Tweets"]
 )
-def post_a_tweet():
-    pass
+def post_a_tweet(tweet: Tweet = Body(...)):
+    """
+    Post a Tweet
+
+    This path operation post a tweet in the app
+
+    Parameters:
+      - Request body parameter
+        - tweet: Tweet
+    
+    Returns a json with the basic tweet information:
+      - tweet_id: UUID
+      - content: str
+      - created_at: datetime  
+      - updated_at: Optional[datetime]
+      - by: User
+    """
+
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+        
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+
+        return tweet
 
 ### Show a Tweet
 @app.get(
